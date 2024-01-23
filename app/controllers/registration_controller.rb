3a57b97335369df15d167539101a218438107cb8
ITDevSuccess <raharisontsidiany@gmail.com>
2024-01-23 16:47:31 +0300

    Fin de formulaire d'enregistrement

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -2,4 +2,21 @@ class RegistrationController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Successfully create account"
+    else
+      # flash[:alert] = "Error !"
+      render :new
+    end
+    # render plain: params[:user]
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
