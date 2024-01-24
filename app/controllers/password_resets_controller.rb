fd838a0b8bc0019beba9cdcd9acf0f262c17a16a
ITDevSuccess <raharisontsidiany@gmail.com>
2024-01-24 09:40:25 +0300

    Fin de auths

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -7,8 +7,29 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user.present?
       UserMailer.with(user: @user).reset.deliver_now
-    else
-      redirect_to root_path, notice: "The email is send !"
     end
+    redirect_to root_path, notice: "The email is send !"
+  end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: "Your token has expires !"
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: "Your password was reset successfully"
+    else
+      render :edit
+    end
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: "Error to reset the password !"
+  end
+
+  private
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
