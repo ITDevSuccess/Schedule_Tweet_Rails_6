c5cd1778a23105361c976f7c0a1fc12230ad4395
ITDevSuccess <raharisontsidiany@gmail.com>
2024-01-24 06:52:12 +0300

    Ajout login

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get "about", to: "about#index"
   get "sign_up", to: "registration#new"
   post "sign_up", to: "registration#create"
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
 end
